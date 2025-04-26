c82c5bdda458fbbbd817f63eae0d07f93dd71615
Alexandr Yermak <ayermak@protonmail.ch>
2025-04-26 13:27:47 +1000

    initial commit

---
+++ nvim/lua/plugins/blink-cmp.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    "saghen/blink.compat",
+    version = "*",
+    event = "InsertEnter",
+    opts = {},
+  },
+  {
+    "saghen/blink.cmp",
+    version = "1.*",
+    event = "InsertEnter",
+    opts = {
+      -- C-space: Open menu or open docs if already open
+      -- C-e: Hide menu
+      -- C-k: Toggle signature help (if signature.enabled = true)
+      --
+      -- See :h blink-cmp-config-keymap for defining your own keymap
+      keymap = {
+        preset = "enter",
+        ["<C-Z>"] = { "accept", "fallback" },
+        ["<C-j>"] = { "select_next", "fallback" },
+        ["<C-k>"] = { "select_prev", "fallback" },
+        ["<C-u>"] = { "scroll_documentation_up", "fallback" },
+        ["<C-d>"] = { "scroll_documentation_down", "fallback" },
+      },
+
+      appearance = {
+        nerd_font_variant = "mono",
+      },
+
+      completion = { documentation = { auto_show = true } },
+      signature = { enabled = true },
+
+      -- Default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, due to `opts_extend`
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
+
+      fuzzy = { implementation = "prefer_rust_with_warning" },
+    },
+    opts_extend = { "sources.default" },
+  },
+}
