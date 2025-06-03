7cf3c8923fecc88d3f8dcef60ac11185a95fd9a2
Alexandr Yermak <ayermak@protonmail.ch>
2025-06-03 16:06:40 +1000

    initial commit

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,33 +3,30 @@ return {
   event = { "BufReadPost", "BufNewFile" },
   cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
   build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      -- https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#supported-languages
-      ensure_installed = {
-        "php",
-        "lua",
-        "svelte",
-        "typescript",
-        "tsx",
-        "css",
-        "scss",
-        "bash",
-        "dockerfile",
-        "gitignore",
-        "go",
-        "json",
-        "astro",
-      },
-      sync_install = false,
-      auto_install = true,
-      highlight = {
-        enable = true,
-        use_languagetree = true,
-      },
-      indent = {
-        enable = true,
-      },
-    })
-  end,
+  opts = {
+    -- https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#supported-languages
+    ensure_installed = {
+      "php",
+      "lua",
+      "svelte",
+      "typescript",
+      "tsx",
+      "css",
+      "scss",
+      "bash",
+      "dockerfile",
+      "gitignore",
+      "go",
+      "json",
+      "astro",
+    },
+    sync_install = false,
+    auto_install = true,
+    highlight = {
+      enable = true,
+      use_languagetree = true,
+    },
+    indent = { enable = true },
+  },
+  config = function(_, opts) require("nvim-treesitter.configs").setup(opts) end,
 }
