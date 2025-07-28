57a776870a8ac1c9fe14c073bee2d107eb307f3d
Alexandr Yermak <ayermak@protonmail.ch>
2025-07-28 12:04:29 +1000

    initial commit

---
+++ nvim/lua/plugins/vigt.lua
@@ -0,0 +1,20 @@
+return {
+  "tanvirtin/vgit.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  event = "VimEnter",
+  keys = {
+    { "<leader>gb", "<cmd>VGit buffer_gutter_blame_preview<cr>", desc = "Blame" },
+    { "<leader>gz", "<cmd>VGit buffer_hunk_reset<cr>", desc = "Reset hunk" },
+    { "<leader>gZ", "<cmd>VGit buffer_reset<cr>", desc = "Reset buffer" },
+    { "<leader>gh", "<cmd>VGit buffer_hunk_preview<cr>", desc = "Preview hunk" },
+    { "<leader>gd", "<cmd>VGit buffer_diff_preview<cr>", desc = "Buffer diff" },
+    { "<leader>gD", "<cmd>VGit project_diff_preview<cr>", desc = "Project diff" },
+  },
+  opts = {
+    settings = {
+      live_blame = { enabled = false },
+    },
+  },
+}
